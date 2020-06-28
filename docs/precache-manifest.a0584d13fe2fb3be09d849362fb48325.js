bc15f8493ff344107d97e7fcb2f258241c566bde
juanfer1 <juan.villadiego@magneto365.com>
2020-06-28 14:32:32 -0500

    Config core

---
+++ docs/precache-manifest.a0584d13fe2fb3be09d849362fb48325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc854b354e83fbf99b9df68a09ac9c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e044c6a5e43d6b69e7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5305574358771fee84c6",
+    "url": "/static/js/2.384fab61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.384fab61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e044c6a5e43d6b69e7",
+    "url": "/static/js/main.79757699.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
